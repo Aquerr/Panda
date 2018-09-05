1d7385db03d09caefa7b46b30815f00f637b01c1
Aquerr <nerdianin@gmail.com>
2018-09-05 19:28:58 +0200

    Added game command

---
+++ src/main/java/io/github/aquerr/pandobot/commands/GameCommand.java
@@ -0,0 +1,24 @@
+package io.github.aquerr.pandobot.commands;
+
+import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
+
+import java.util.List;
+
+public class GameCommand implements ICommand
+{
+    @Override
+    public boolean execute(User user, MessageChannel channel, List<String> args)
+    {
+        if(args.size() != 1)
+        {
+            channel.sendMessage("Błąd! Zła ilość wymaganych argumentów! Wymagana ilość argumentów: 1").queue();
+            return false;
+        }
+
+        channel.getJDA().getPresence().setGame(Game.of(Game.GameType.DEFAULT, args.get(0)));
+
+        return false;
+    }
+}
